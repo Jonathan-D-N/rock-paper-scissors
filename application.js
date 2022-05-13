5563ac51c6f70fe4f0f53c1dcd94df8a359f1e03
Jonathan N <jdneave@gmail.com>
2022-05-13 17:18:53 -0600

    add basic pseudocode to application.js

---
+++ application.js
@@ -0,0 +1,20 @@
+//objectives: Create a rock paper scissors game played against the computer
+
+//Pseudocode
+
+
+
+// Assign function: computerPlay 
+// computerPlay includes variables: rock , paper , scissors
+// function: computerPlay activates upon user input from the: playerPlay function
+// Opon user input computerPlay selects one of the three variables at random and returns the variable computerSelection
+
+// assign function: playerPlay
+// playerPlay includes variables: rock , paper , scissors
+// User inputs one of 3 values: rock , paper , scissors (case insensitive)
+// function returns variable: playerSelection
+
+// assign function: runGame
+// runGame includes variables: playerSelection , computerSelection
+// runGame compares values of inputs from both variables
+// declares a win , lose , or draw to the user based on input from playerSelection and computerSelection
