1d3b6bf169fa0c67e595f26bb7ad2ec8707fc2b3
Jonathan N <jdneave@gmail.com>
2022-05-13 22:59:30 -0600

    fix bug in playerPlay function

--- application.js
+++ application.js
@@ -1,21 +1,3 @@
-//objectives: Create a rock paper scissors game played against the computer
-
-//Pseudocode
-
-// Assign function: computerPlay 
-// computerPlay includes variables: rock , paper , scissors
-// function: computerPlay activates upon user input from the: playerPlay function
-// Upon user input computerPlay selects one of the three variables at random and returns the variable computerSelection
-
-// assign function: playerPlay
-// playerPlay includes variables: rock , paper , scissors
-// User inputs one of 3 values: rock , paper , scissors (case insensitive)
-// function returns variable: playerSelection
-
-// assign function: runGame
-// runGame includes variables: playerSelection , computerSelection
-// runGame compares values of inputs from both variables
-// declares a win , lose , or draw to the user based on input from playerSelection and computerSelection
 
 //global variables
 let playerPoint = (0);
@@ -61,7 +43,7 @@ function playerPlay() {
             //return console.log('Player: ' + playerSelection) + console.log('computer: ' + computerSelection);
         } else {
             console.log(`'${playerOutput}' is not a valid input. Please input "rock", "paper", or "scissors"`);
-            
+            game();
         }    
 }
 // compares results of playerSelection and computerSelection, outputs: computerPoint or playerPoint
