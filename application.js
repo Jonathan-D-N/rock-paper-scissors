8fd6f43bdf22b4134880155990f337f61470e77d
Jonathan <jdneave@gmail.com>
2022-08-20 16:39:48 -0600

    rewrote entire game to run within 1 function, no global variable

--- application.js
+++ application.js
@@ -1,50 +1,53 @@
-
-//global variables
-let playerPoint = (0);
-let computerPoint = (0);
-let rounds = (1);
-let playerSelection;
-let computerSelection;
-
-// Declares a random value, 'rock', 'paper', or 'scissors' for the computer player. Call using the computerPlay() function.
-const gameValues = ['rock', 'paper', 'scissors']; 
-
-function computerPlay() {
-    return gameValues[~~(Math.random() * gameValues.length)];
+//This function runs the game
+function game(){
+    const gameValues = ["rock", "paper", "scissors"];
+    let playerPoint = (0);
+    let computerPoint = (0);
+    let playerSelection;
+    let computerSelection;
+    return playerPlay()
+//This function randomizes the computer's selection from gameValues
+   // function computerRandom() {
+     //   const gameValues = ["rock", "paper", "scissors"] * [~~(Math.random() * gameValues.length)];
+   // }
+//This function takes user input, forces lowercase, randomizes gameValues for computer selection
+    function playerPlay(computerSelection, playerSelection){
+        playerSelection = prompt('Input "rock", "paper", or "scissors"')
+        playerSelection = playerSelection.toLowerCase();
+        computerSelection = (gameValues[~~(Math.random() * gameValues.length)]);
+            if (computerSelection == playerSelection) {
+                console.log(`You picked ${playerSelection}, the computer picked ${computerSelection}. It's a draw!`);
+                return (playerPlay());
+            }
+            //computer win
+            else if (computerSelection == 'rock' && playerSelection == 'scissors' || computerSelection == 'paper' && playerSelection == 'rock' || computerSelection == 'scissors' && playerSelection == 'paper'){
+                computerPoint = ++computerPoint;
+                console.log('You lose! ' + computerSelection + ' beats ' + playerSelection + '!');
+                return (loopGame());
+            }
+            //player win
+            else if (playerSelection == 'rock' && computerSelection == 'scissors' || playerSelection == 'paper' && computerSelection == 'rock' || playerSelection == 'scissors' && computerSelection == 'paper'){
+                playerPoint = ++playerPoint;
+                console.log('You win! ' + playerSelection + ' beats ' + computerSelection + '!')
+                return (loopGame());
+            }
+            //invalid input from player
+            else {
+                console.log(`${playerSelection } is not a valid input. Please input "rock", "paper", or "scissors"`)
+                return (playerPlay());
+            }
     }
-
-//Take player input, force lowercase, compare results, add points for winner and add to rounds variable, loop function if a draw is met.
-function playerPlay(computerSelection, playerSelection,) {
-    playerSelection = prompt('Input "rock", "paper", or "scissors"')
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerPlay().toLowerCase();
-        if (computerSelection == playerSelection) {
-            console.log('Draw!');
+    function loopGame() {
+        console.log('Player points: ' + playerPoint)
+        console.log('Computer points: ' + computerPoint)
+        for(;computerPoint < (3) && playerPoint < (3);) {
             return (playerPlay());
         }
-        //Computer win
-        else if (computerSelection == 'rock' && playerSelection == 'scissors' || computerSelection == 'paper' && playerSelection == 'rock' || computerSelection == 'scissors' && playerSelection == 'paper') {
-            computerPoint = ++computerPoint;
-            rounds = ++rounds;
-            console.log('You lose! ' + computerSelection + ' beats ' + playerSelection + '!');
-            console.log('Player Points: ', playerPoint);
-            console.log('Computer Points: ', computerPoint);
-        //Player win
-        } else if (playerSelection == 'rock' && computerSelection == 'scissors' || playerSelection == 'paper' && computerSelection == 'rock' || playerSelection == 'scissors' && computerSelection == 'paper'){
-            playerPoint = ++playerPoint;
-            rounds = ++rounds;
-            console.log('You win! ' + playerSelection + ' beats ' + computerSelection + '!');
-            console.log('Player Points: ', playerPoint);
-            console.log('Computer Points: ', computerPoint);
-        } else {
-            console.log(`${playerSelection } is not a valid input. Please input "rock", "paper", or "scissors"`)
-            return (playerPlay());
+        if (playerPoint >= (3)) {
+            console.log('You won best 3 out of 5!')
         }
-    } 
-//runs the game until 5 rounds happen
-function game() {
-   while (playerPoint < (3) && computerPoint < (3)) {
-       playerPlay();
+        else {
+            console.log('Computer won best 3 out of 5. Better luck next time!')
+        }
+        }
     }
-}
-game()
