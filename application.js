bd40e86dde2d1768060ed0981b0291e9643d6e64
Jonathan <jdneave@gmail.com>
2022-11-12 20:16:19 -0700

    finished working version of game w/ no if statements.

--- application.js
+++ application.js
@@ -1,43 +1,29 @@
 const picks = ['rock', 'paper', 'scissors']
+//User Choice
+let userInput = prompt('Input "rock", "paper", or "scissors"').toLowerCase();
+let userInputIndex = picks.indexOf(userInput);
 
+//Computer choice
+let randomNum = Math.floor(Math.random() * 3);
+let computerChoice = picks[randomNum];
 
-//Write a function with 2 arguments that will be passed in to it. 1st argument will hold the player input, 2nd will hold the computer input.
-//Upon running this function, prompt the user to input text. Store the text on 1st argument and run the next function.
-//the next function will randomize the 'picks' variable and store the information on the 2nd argument. After doing this, run the next (3rd) function.
-//3rd function will take argument 1 and 2 and compare them. Once compared, it will console.log the winner.
+//Log choices to the console
+console.log(`You picked ${userInput}, the computer picked ${computerChoice}`);
 
+//2D array
+    const results = [
+        ['T', 'W', 'L'],
+        ['L', 'T', 'W'],
+        ['W', 'L', 'T'],
+    ];
+    console.log(results);
+//
+let userResult = results[randomNum][userInputIndex];
+//console.log('UserResult',userResult)
 
-//Begins the game. Prompts for userInput and forces lower case, then runs InputValidity function.
-function runGame () {
-    userInput = prompt('Input "rock", "paper", or "scissors"').toLowerCase();
-    inputValidity()
-    //Conditional branching to determine valid input. If no valid input, run  the game again.
-    function inputValidity() {
-        //If input is invalid, inform user and run the game again.
-        function invalidInput() {
-            console.log(`${userInput} is not a valid input. Please type "rock", "paper", or "scissors"`)
-            runGame()
-        }
-        (userInput == "rock" ? console.log(userInput)
-                : userInput == "paper" ? console.log(userInput)
-                : userInput == "scissors" ? console.log(userInput)
-                : invalidInput())
-    //}
-    //const results = [
-    //    ['W', 'L', 'T'],
-    //    ['T', 'W', 'L'],
-    //    ['L', 'T', 'W'],
-    //];
-    //runs the computerChoice function, passing the 'picks' variable as the argument. The function will randomize that variable (array) and return 1 randomized output.
-    computerChoice(picks)
-    //This function runs the Durstenfeld shuffle to shuffle the array. I'm then calling the array position 0 which will be compared to userinput.
-    //May not be appropriate for this project. May need to adjust to just a: let randomNum = Math.floor(Math.random() * 3);
-    function computerChoice(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            let j = Math.floor(Math.random() * (i + 1));
-            let temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-        } //console.log (array[0])
-    }
-}}
+const resultMap = {
+    'T': "Tie",
+    'W': "You win",
+    'L': "You lose"
+};
+console.log(resultMap[userResult]);
