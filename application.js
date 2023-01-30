bb3d846b5114e0b7b970769324b3fe21c05ad7e3
Jonathan <jdneave@gmail.com>
2023-01-30 12:14:41 -0700

    add onClick animations function

--- application.js
+++ application.js
@@ -1,3 +1,24 @@
-let playerScore = 0
-let computerScore = 0
-
+// Page load animations
+const hoverPlay = document.querySelector('.play');
+hoverPlay.onclick = () => {
+    const playElement = document.querySelector('.play')
+    const clickLight1 = document.querySelector('.clickLight1')
+    const clickDark = document.querySelector('.clickDark2')
+    const clickPlay = document.querySelector('.clickPlay')
+    const onClick = document.querySelector('.onClickAnimation')
+    const container = document.querySelector('.container')
+    onClick.setAttribute("id", "onClickAppear")
+    playElement.setAttribute("id", "playVanish")
+    clickLight1.setAttribute("id", "lightClicked")
+    setTimeout(() => {
+        clickDark.setAttribute("id", "darkClicked")
+        clickPlay.setAttribute("id", "playClicked")
+    }, 100);
+    setTimeout(() => {
+       hoverPlay.remove()
+       onClick.remove()
+    }, 400);
+    setTimeout(() => {
+        container.setAttribute("id", "containerAppear")
+     }, 200);
+}
