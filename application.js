d1e673d78de7619899d7a6092a0000dbacbacbf5
Jonathan <jdneave@gmail.com>
2023-01-30 15:29:17 -0700

    refining animation timings

--- application.js
+++ application.js
@@ -1,24 +1,35 @@
 // Page load animations
-const hoverPlay = document.querySelector('.play');
-hoverPlay.onclick = () => {
-    const playElement = document.querySelector('.play')
+const clickableElement = document.querySelector('.hover');
+
+clickableElement.onclick = () => {
+    const instructionsElement = document.querySelector('.instructions')
+    const hoveredElement = document.querySelector('.hover')
     const clickLight1 = document.querySelector('.clickLight1')
     const clickDark = document.querySelector('.clickDark2')
-    const clickPlay = document.querySelector('.clickPlay')
     const onClick = document.querySelector('.onClickAnimation')
     const container = document.querySelector('.container')
     onClick.setAttribute("id", "onClickAppear")
-    playElement.setAttribute("id", "playVanish")
+    hoveredElement.setAttribute("id", "playVanish")
     clickLight1.setAttribute("id", "lightClicked")
     setTimeout(() => {
         clickDark.setAttribute("id", "darkClicked")
-        clickPlay.setAttribute("id", "playClicked")
     }, 100);
     setTimeout(() => {
-       hoverPlay.remove()
+       clickableElement.remove()
        onClick.remove()
     }, 400);
     setTimeout(() => {
-        container.setAttribute("id", "containerAppear")
+        instructionsElement.setAttribute("id", 'instructionsAnimate')
+        console.log('test')
+        container.setAttribute("id", "containerAppear");
      }, 200);
+}
+moveText()
+function moveText() {
+    const text = document.querySelector('.title')
+    setTimeout(() => {
+        text.style.transform = "translateY(0%)";
+        text.style.transform = "scale(100%)";
+        clickableElement.style.opacity = "100";
+     }, 4200);
 }
