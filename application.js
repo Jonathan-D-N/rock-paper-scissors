b69501139c872684903a464cdf4387e3f252d131
Jonathan <jdneave@gmail.com>
2023-01-30 16:25:37 -0700

    finishing touches on animations

--- application.js
+++ application.js
@@ -1,29 +1,62 @@
 // Page load animations
 const clickableElement = document.querySelector('.hover');
-
+//Game Container
+const rock = document.querySelector('.rock')
+const paper = document.querySelector('.paper')
+const scissors = document.querySelector('.scissors')
+//Menu Container
+const player = document.querySelector('.player')
+const reset = document.querySelector('.reset')
+const computer = document.querySelector('.computer')
+//
+const gameContainer = document.querySelector('.gameContainer')
+const temp = document.querySelector('.gameContainer')
+gameContainer.remove()
 clickableElement.onclick = () => {
+    //Container
+    const container = document.querySelector('.container')
+    //Instructions container
     const instructionsElement = document.querySelector('.instructions')
+    //Play button elements
     const hoveredElement = document.querySelector('.hover')
     const clickLight1 = document.querySelector('.clickLight1')
     const clickDark = document.querySelector('.clickDark2')
     const onClick = document.querySelector('.onClickAnimation')
-    const container = document.querySelector('.container')
     onClick.setAttribute("id", "onClickAppear")
     hoveredElement.setAttribute("id", "playVanish")
     clickLight1.setAttribute("id", "lightClicked")
+    clickableElement.remove()
     setTimeout(() => {
         clickDark.setAttribute("id", "darkClicked")
     }, 100);
     setTimeout(() => {
-       clickableElement.remove()
        onClick.remove()
     }, 400);
     setTimeout(() => {
         instructionsElement.setAttribute("id", 'instructionsAnimate')
-        console.log('test')
         container.setAttribute("id", "containerAppear");
+        container.append(gameContainer)
      }, 200);
+     setTimeout(() => {
+        rock.setAttribute("id", "rock")
+     }, 500);
+     setTimeout(() => {
+        player.setAttribute("id", "player")
+     }, 650);
+     setTimeout(() => {
+        paper.setAttribute("id", "paper")
+     }, 800);
+     setTimeout(() => {
+        reset.setAttribute("id", "reset")
+     }, 950);
+     setTimeout(() => {
+        scissors.setAttribute("id", "scissors")
+     }, 1100);
+     setTimeout(() => {
+        computer.setAttribute("id", "computer")
+     }, 1250);
 }
+//Adjust Rock Paper Scissors title text
 moveText()
 function moveText() {
     const text = document.querySelector('.title')
