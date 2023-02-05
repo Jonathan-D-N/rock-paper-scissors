49367ebc841e42785c800fbe31e2cb99ca5e1e8a
Jonathan <jdneave@gmail.com>
2023-02-04 18:10:00 -0700

    refactoring

--- animations.js
+++ animations.js
@@ -1,77 +1,77 @@
 // Page load animations
 const clickableElement = document.querySelector('.hover');
-const unClickableElement = document.querySelector('.animations')
-//Container
-const winLose = document.querySelector('.winLoseContainer')
-//Game Container
-const rock = document.querySelector('.rock')
-const paper = document.querySelector('.paper')
-const scissors = document.querySelector('.scissors')
-//Menu Container
-const menu = document.querySelector('.menu')
-const player = document.querySelector('.player')
-const reset = document.querySelector('.reset')
-const computer = document.querySelector('.computer')
+const unClickableElement = document.querySelector('.animations');
+// Container
+const winLose = document.querySelector('.winLoseContainer');
+// Game Container
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+// Menu Container
+const menu = document.querySelector('.menu');
+const player = document.querySelector('.player');
+const reset = document.querySelector('.reset');
+const computer = document.querySelector('.computer');
 //
-const gameContainer = document.querySelector('.gameContainer')
-const temp = document.querySelector('.gameContainer')
-gameContainer.remove()
+const gameContainer = document.querySelector('.gameContainer');
+const temp = document.querySelector('.gameContainer');
+gameContainer.remove();
 clickableElement.onclick = () => {
-    //Container
-    const container = document.querySelector('.container')
-    //Instructions container
-    const instructionsElement = document.querySelector('.instructions')
-    //Play button elements
-    const hoveredElement = document.querySelector('.hover')
-    const clickLight1 = document.querySelector('.clickLight1')
-    const clickDark = document.querySelector('.clickDark2')
-    const onClick = document.querySelector('.onClickAnimation')
-    onClick.setAttribute("id", "onClickAppear")
-    hoveredElement.setAttribute("id", "playVanish")
-    clickLight1.setAttribute("id", "lightClicked")
-    clickableElement.remove()
-    setTimeout(() => {
-        clickDark.setAttribute("id", "darkClicked")
-    }, 100);
-    setTimeout(() => {
-       onClick.remove()
-    }, 400);
-    setTimeout(() => {
-        instructionsElement.setAttribute("id", 'instructionsAnimate')
-        container.setAttribute("id", "containerAppear");
-        container.append(gameContainer)
-     }, 500);
-     setTimeout(() => {
-        rock.setAttribute("id", "rock")
-     }, 800);
-     setTimeout(() => {
-        player.setAttribute("id", "player")
-     }, 950);
-     setTimeout(() => {
-        paper.setAttribute("id", "paper")
-     }, 1100);
-     setTimeout(() => {
-        reset.style.transition = "all 1000ms ease-in"
-        reset.setAttribute("id", "reset")
-     }, 1250);
-     setTimeout(() => {
-        scissors.setAttribute("id", "scissors")
-     }, 1400);
-     setTimeout(() => {
-        computer.setAttribute("id", "computer")
-     }, 1550);
-     setTimeout(() => {
-      reset.style.transition = "50ms"
-   }, 2250);
-}
-//Adjust Rock Paper Scissors title text location and scale
-moveText()
+  // Container
+  const container = document.querySelector('.container');
+  // Instructions container
+  const instructionsElement = document.querySelector('.instructions');
+  // Play button elements
+  const hoveredElement = document.querySelector('.hover');
+  const clickLight1 = document.querySelector('.clickLight1');
+  const clickDark = document.querySelector('.clickDark2');
+  const onClick = document.querySelector('.onClickAnimation');
+  onClick.setAttribute('id', 'onClickAppear');
+  hoveredElement.setAttribute('id', 'playVanish');
+  clickLight1.setAttribute('id', 'lightClicked');
+  clickableElement.remove();
+  setTimeout(() => {
+    clickDark.setAttribute('id', 'darkClicked');
+  }, 100);
+  setTimeout(() => {
+    onClick.remove();
+  }, 400);
+  setTimeout(() => {
+    instructionsElement.setAttribute('id', 'instructionsAnimate');
+    container.setAttribute('id', 'containerAppear');
+    container.append(gameContainer);
+  }, 500);
+  setTimeout(() => {
+    rock.setAttribute('id', 'rock');
+  }, 800);
+  setTimeout(() => {
+    player.setAttribute('id', 'player');
+  }, 950);
+  setTimeout(() => {
+    paper.setAttribute('id', 'paper');
+  }, 1100);
+  setTimeout(() => {
+    reset.style.transition = 'all 1000ms ease-in';
+    reset.setAttribute('id', 'reset');
+  }, 1250);
+  setTimeout(() => {
+    scissors.setAttribute('id', 'scissors');
+  }, 1400);
+  setTimeout(() => {
+    computer.setAttribute('id', 'computer');
+  }, 1550);
+  setTimeout(() => {
+    reset.style.transition = '50ms';
+  }, 2250);
+};
+// Adjust Rock Paper Scissors title text location and scale
+moveText();
 function moveText() {
-    const text = document.querySelector('.title')
+  const text = document.querySelector('.title');
     setTimeout(() => {
-        text.style.transform = "translateY(0%)";
-        text.style.transform = "scale(100%)";
-        clickableElement.style.opacity = "100";
-        unClickableElement.removeAttribute("id", "unClickableAnimation")
-     }, 4200);
+    text.style.transform = 'translateY(0%)';
+    text.style.transform = 'scale(100%)';
+    clickableElement.style.opacity = '100';
+    unClickableElement.removeAttribute('id', 'unClickableAnimation');
+  }, 4200);
 }
